3ae370ce13b0b9c587b78634003330d7c0ca7fc4
WeiDer <weiderwork131491@gmail.com>
2022-12-25 17:46:48 +0800

    routing 設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './home/about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './home/product/product.component';
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full", // 當路徑是空的時候轉址到 home
+  },
+  {
+    path: "",
+    component: HomeComponent,
+    data:{
+      title:"首頁"
+    },
+    children: [
+      {
+        path:"",
+        component: HomeComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      {
+        path: 'product',
+        component: ProductComponent,
+      },
+    ]
+  },
+  {
+    path: "**", component: HomeComponent, // 萬用路徑，路由沒有比對到，永遠會執行
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './home/about/about.component';
+import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './home/product/product.component';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    LoginComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
