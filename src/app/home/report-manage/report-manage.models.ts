2c2dbd0398898b62acacaefb8c12aa234e368d1d
WeiDer <weiderwork131491@gmail.com>
2023-08-25 00:22:34 +0800

    add 搜尋欄位

--- src/app/home/report-manage/report-manage.models.ts
+++ src/app/home/report-manage/report-manage.models.ts
@@ -6,7 +6,13 @@ export interface exportSampleManageModels {
   /**目標廣告 */
   goalAds: string;
   /**媒體類型 */
-  mediaType: string;
+  mediaType: media;
   /**修改時間 */
   creatDt: string;
+}
+/**媒體類型 */
+export enum media {
+  Google="Google",
+  FB="FB",
+  IG="IG"
 }
